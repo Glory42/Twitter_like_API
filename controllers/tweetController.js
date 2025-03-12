08a242b0d884ef4ed53a345fa01e729fbe9e41fc
Glory42 <gorkemkaryol03@gmail.com>
2025-03-12 14:06:25 +0300

    Made changes on Controller: added createTweet functionality and exported.

---
+++ controllers/tweetController.js
@@ -0,0 +1,48 @@
+const pool  = require('../config/db');
+
+const extractHashtags = (content) => {
+    const hashtags = content.match(/#\w+/g) || [];
+    return [...new Set(hashtags.map(tag => tag.slice(1).toLowerCase()))];
+};
+
+const createTweet = async (req, res) => {
+    const { content } = req.body;
+    const userId = req.user.id;
+
+    if (!content) {
+        return res.status(400).json({ error: 'Tweet content is required' });
+    };
+
+    try {
+        await pool.query('BEGIN');
+
+        const tweet = await pool.query(
+            'INSERT INTO tweets(user_id, content) VALUES ($1, $2) RETURNING *',
+            [userId, content]
+        );
+
+        const hashtags = extractHashtags(content);
+        for (const tag of hashtags) {
+            const hashtags = await pool.query(
+                'INSERT INTO hashtags(tag) VALUES($1) ON CONFLICT(tag) DO NOTHING RETURNING *',
+                [tag]
+            );
+
+            if (hashtags.rows[0]) {
+                await pool.query(
+                    'INSERT INTO tweet_hashtags(tweet_id, hashtag_id) VALUES ($1, $2)',
+                    [tweet.rows[0].id, hashtags.rows[0].id]
+                );
+            }
+        }
+        await pool.query('COMMIT');
+        res.status(201).json(tweet.rows[0]);
+    } catch (err) {
+        await pool.query('ROLLBACK');
+        res.status(500).json({ error: 'Failed to post tweet' });
+    }
+};
+
+module.exports = {
+    createTweet
+};
